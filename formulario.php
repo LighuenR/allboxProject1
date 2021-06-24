1ae57c5dd1b780d20bf10cafeb8e0ba003976fbb
Lighuen Restanio <lighuen.restanio@gmail.com>
2021-06-24 16:22:51 -0300

    php first try

--- formulario.php
+++ formulario.php
@@ -1,1 +1,12 @@
-<php>
+<?php 
+	$name = $_POST['nombre'];
+	$email = $_POST['email'];
+	$tel = $_POST['tel'];
+	$mensaje = $_POST['mensaje'];
+
+	echo $name. "Escribio <br/>".$mensaje;
+	if(mail('allbox@gmail.com', $email, $tel, $mensaje)){
+		echo "Mail enviado";
+	}else{
+		echo "No enviado"
+ ?>
