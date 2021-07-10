8d2ec0ff32efcf934fbfc97947b55405af312651
LighuenR <lighuen.restanio@gmail.com>
2021-07-10 10:35:44 -0300

    php first try

--- formulario.php
+++ formulario.php
@@ -1,12 +1,17 @@
-<?php 
+<?php 	
+	$mySpaceEmail = "correogratuito@dt.am"
+	$myPersonalEmail = "allbox@gmail.com"
+
+	if(isset($_POST["submit"])){
 	$name = $_POST['nombre'];
 	$email = $_POST['email'];
 	$tel = $_POST['tel'];
 	$mensaje = $_POST['mensaje'];
 
-	echo $name. "Escribio <br/>".$mensaje;
-	if(mail('allbox@gmail.com', $email, $tel, $mensaje)){
+	echo $name. "Escribio <br/>".$mensaje;}
+	else if(mail('allbox@gmail.com', $email, $tel, $mensaje)){
 		echo "Mail enviado";
 	}else{
 		echo "No enviado"
+		}
  ?>
